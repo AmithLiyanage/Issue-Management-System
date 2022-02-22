d5e6fc587d17be549371dfb9d4e69e4c791a17e7
Amith Liyanage <amith.liyanage.9@gmail.com>
2022-02-22 13:59:42 +0530

    First Commit entgra

---
+++ src/test/java/com/nvoi/Issuemanagementsystem/IssueManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nvoi.Issuemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssueManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
